334e80a021d7784c2c2c9bc83440e25edcfe5498
CJcode6754 <ceejayibabiosa@gmail.com>
2025-04-19 14:54:42 +0800

    Finish setting up the Eloquent model connection

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -20,7 +22,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'phone',
+        'google_id',
+        'facebook_id',
+        'password'
     ];
 
     /**
@@ -44,5 +49,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function favoriteCars():BelongsToMany
+    {
+        return $this->belongsToMany(Car::class, 'favorite_cars', 'user_id', 'car_id');
+    }
+
+    public function cars(): HasMany
+    {
+        return $this->hasMany(Car::class);
     }
 }
