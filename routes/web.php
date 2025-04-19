7424fa0200fef0e07cd1cd85683dbd7e72f6b535
CJcode6754 <ceejayibabiosa@gmail.com>
2025-04-19 14:48:50 +0800

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class,'index']);
+Route::get('/', [DashboardController::class,'index'])->name('dashboard');
+Route::get('/inventory', [DashboardController::class,'show'])->name('inventory');
 
-Route::get('/hello', [DashboardController::class, 'welcome']);
+Route::get('car/search', [CarController::class, 'search'])->name('car.search');
+Route::get('car/watchlist', [CarController::class, 'watchlist'])->name('car.watchlist');
+Route::resource('car', CarController::class);
+Route::get('signup', [SignupController::class, 'create'])->name('signup');
+
+Route::get('login', [LoginController::class, 'login'])->name('login');
