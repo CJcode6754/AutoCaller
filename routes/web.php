040d111bd98458ac281bcd4956fd402c47622c35
CJcode6754 <ceejayibabiosa@gmail.com>
2025-04-20 17:14:10 +0800

    add new routes for auth

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class,'index'])->name('dashboard');
-Route::get('/inventory', [DashboardController::class,'show'])->name('inventory');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::view('/register','auth.register')->name('register');
+Route::view('/login', 'auth.login')->name('login');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [DashboardController::class, 'logout'])->name('logout');
+
+
+Route::get('/inventory', [DashboardController::class, 'show'])->name('inventory');
 
 Route::get('car/search', [CarController::class, 'search'])->name('car.search');
 Route::get('car/watchlist', [CarController::class, 'watchlist'])->name('car.watchlist');
 Route::resource('car', CarController::class);
-Route::get('signup', [SignupController::class, 'create'])->name('signup');
-
-Route::get('login', [LoginController::class, 'login'])->name('login');
