fd42f004b40d7c041b797db9ce02ae3147e22779
CJcode6754 <ceejayibabiosa@gmail.com>
2025-04-21 16:05:17 +0800

    Implement middleware

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,21 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::view('/register','auth.register')->name('register');
-Route::view('/login', 'auth.login')->name('login');
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [DashboardController::class, 'logout'])->name('logout');
 
+Route::middleware(['guest'])->group(function () {
 
-Route::get('/inventory', [DashboardController::class, 'show'])->name('inventory');
+    Route::view('/register','auth.register')->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('car/search', [CarController::class, 'search'])->name('car.search');
-Route::get('car/watchlist', [CarController::class, 'watchlist'])->name('car.watchlist');
-Route::resource('car', CarController::class);
+    Route::view('/login', 'auth.login')->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [DashboardController::class, 'logout'])->name('logout');
+
+    Route::get('car/search', [CarController::class, 'search'])->name('car.search');
+    Route::get('car/watchlist', [CarController::class, 'watchlist'])->name('car.watchlist');
+    Route::resource('car', CarController::class);
+});
+
