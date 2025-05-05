27ecde8f43c8ae3cc51184cf321ee266d6c0d568
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-05 20:24:33 +0800

    refactor: delete and fix the footer

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -4,18 +4,10 @@
             <h2 class="text-white text-4xl font-semibold">Join Cars</h2>
             <p class="text-white text-sm font-light">Receive pricing updates, shopping tips & more!</p>
 
-            <div class="flex items-center space-x-4 mt-4">
-                <form action="" method="">
-                    <input type="text" name="email" placeholder="Enter your email" class="text-white px-6 mr-2 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <button class="text-center text-white bg-blue-500 hover:bg-blue-700 py-2 px-6 rounded-lg transition  duration-150">Sign Up</button>
-                </form>
-            </div>
-
             <hr class="w-full border-gray-300 my-4">
         </div>
-        <div class="flex items-center justify-evenly mx-0 md:mx-40 pt-6">
+        <div class="flex items-center justify-center mx-0 md:mx-40 pt-6">
             <h3 class="text-white text-sm font-medium">© 2025 cars.com. All rights reserved.</h3>
-            <h3 class="text-white text-sm font-medium">Terms & Conditions</h3>
         </div>
     </section>
 </Footer>
