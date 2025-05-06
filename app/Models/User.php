256b370ad9aad7bc310f9babb94687b182ea93f8
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-06 16:31:41 +0800

    feat: add show and edit profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,8 @@ class User extends Authenticatable implements MustVerifyEmail, CanResetPassword
         'phone',
         'google_id',
         'facebook_id',
-        'password'
+        'password',
+        'profile_pic'
     ];
 
     /**
